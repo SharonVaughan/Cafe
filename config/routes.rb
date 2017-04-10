430365ac611e332c568ab1cb658c7e8b394342aa
Sharon <sharon-vaughan@bigpond.com>
2017-04-10 11:30:34 +1000

    Feat: Add item
    Body: Migrated item controll with a title and price

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :items  #creates a get and heaps of other stuff
 	get '/menu' => 'menu#index'  #removes /index now only localhost3000/menu
   # get '/menu', to: 'menu#index'
   # get 'menu/index'
